4632d68d4f3cae668ec9f8c051a4f251cf2ec7aa
steffen <neffets720126@web.de>
2025-01-04 14:55:05 +0100

    Prevent flickering

--- calendar.bat
+++ calendar.bat
@@ -8,7 +8,14 @@ using System.Threading.Tasks;^
 using System.Windows.Forms;^
 public class DLForm:Form{^
   [DllImport(\"dwmapi.dll\")] static extern int DwmSetWindowAttribute(IntPtr w,int a,ref int v,int s);^
-  [DllImport(\"shcore.dll\")] static extern int SetProcessDpiAwareness(int v);^
+  [DllImport(\"user32.dll\")] static extern int SetThreadDpiAwarenessContext(int c);^
+  public void SuspendRedrawWhile(Action act){^
+    Message m=Message.Create(Handle,11,IntPtr.Zero,IntPtr.Zero);^
+    DefWndProc(ref m);^
+    m.WParam=(IntPtr)1;^
+    try{act();}^
+    finally{DefWndProc(ref m); Invalidate(); Refresh();}^
+  }^
   bool dM;^
   void UpdtDM(){^
     DarkMode=(0==(int)Registry.GetValue(@\"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize\",\"AppsUseLightTheme\",1));^
@@ -20,7 +27,7 @@ public class DLForm:Form{^
   bool dTB;^
   public bool DarkTitleBar{^
     get{return dTB;}^
-    set{int v=value?1:0; if(0==DwmSetWindowAttribute(this.Handle,20,ref v,4)) dTB=value;}^
+    set{int v=value?1:0; if(0==DwmSetWindowAttribute(Handle,20,ref v,4)) dTB=value;}^
   }^
   public event Action DarkModeChanged=delegate{};^
   protected override void WndProc(ref Message m){^
@@ -28,7 +35,7 @@ public class DLForm:Form{^
     if(m.Msg==26 ^&^& m.WParam==IntPtr.Zero ^&^& Marshal.PtrToStringAuto(m.LParam)==\"ImmersiveColorSet\") Task.Run(()=^>{UpdtDM(); DarkModeChanged();});^
   }^
   public DLForm(){^
-    SetProcessDpiAwareness(2);^
+    SetThreadDpiAwarenessContext(-4);^
     UpdtDM();^
   }^
 }' -R System.Windows.Forms;^
@@ -48,16 +55,17 @@ function Show-DLForm{^
   $sdlf_Ps.Dispose();^
 }^
 $wnd=[DLForm]@{AutoSize=$true; FormBorderStyle='Fixed3D'; Icon=[Drawing.Icon]::ExtractAssociatedIcon($PSHOME+'\powershell.exe'); MaximizeBox=$false; Text='Calendar'};^
-$wnd.add_Shown({$cal.Left=($wnd.ClientSize.Width-$cal.Width)/2; $cal.Top=$chk.Height});^
+$wnd.add_Shown({$cal.Left=($wnd.ClientSize.Width-$cal.Width)/2; $cal.Top=$chk.Height; $wnd.Activate()});^
 $chk=[Windows.Forms.CheckBox]@{AutoSize=$true; Padding='20,10,0,20'; Text='Always on top'};^
 $chk.add_CheckedChanged({$wnd.TopMost=$chk.Checked});^
 $cal=[Windows.Forms.MonthCalendar]@{CalendarDimensions='3,4'; Margin='0,0,0,20'; ScrollChange=3; ShowToday=$false; ShowWeekNumbers=$true};^
 $wnd.Controls.AddRange(@($chk,$cal));^
 $settheme={^
   if($wnd.DarkMode){$thm='#3F4144','#1F2023','White','#CFD1D4','Black'}else{$thm='#DFE0E3','#EFF0F2','Black','#3F4144','White'}^
-  $cal.BackColor=$thm[1]; $cal.ForeColor=$thm[2]; $cal.TitleBackColor=$thm[3]; $cal.TitleForeColor=$thm[4];^
-  $chk.ForeColor=$thm[2];^
-  $wnd.BackColor=$thm[0]; $wnd.DarkTitleBar=$wnd.DarkMode^
+  $wnd.SuspendRedrawWhile({^
+    $cal.ForeColor=$thm[2]; $cal.TitleForeColor=$thm[4]; $chk.ForeColor=$thm[2];^
+    $cal.BackColor=$thm[1]; $cal.TitleBackColor=$thm[3]; $wnd.BackColor=$thm[0]; $wnd.DarkTitleBar=$wnd.DarkMode^
+  });^
 };^
 &$settheme;^
 Show-DLForm $wnd $settheme;^"
